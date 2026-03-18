ccac48e2cf77ba99bc7febb75fee7345920461ee
Isabele Kist <isabelekist022@gmail.com>
2026-03-18 17:52:04 -0300

    Delete FactoryPequeno directory

--- FactoryPequeno/main.py
+++
@@ -1,9 +0,0 @@
-from notFactory import notificacaoFac
-print("TIPOS: email | sms | push")
-
-tipo= input("Escolha o tipo de notificacao: ")
-
-notificacao= notificacaoFac.enviar(tipo)
-mensagem= input("Digite a mensagem: ")
-
-notificacao.enviar(mensagem)
